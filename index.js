8ca5d411853c1a311acbc11831b0c989f303d319
Sam <siacavazzi@gmail.com>
2023-07-06 19:55:56 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+}
+
+function clickAlert() {
+  alert('I was clicked!');
 }
